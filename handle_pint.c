f742a2402931928a5d03677b1e939da5e01665b8
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-25 18:12:45 +0300

    format error messages correctly

--- handle_pint.c
+++ handle_pint.c
@@ -10,7 +10,7 @@ void handle_pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
--- handle_push.c
+++ handle_push.c
@@ -19,7 +19,7 @@ void handle_push(stack_t **stack, unsigned int line_number)
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	new->n = atoi(op_value);
